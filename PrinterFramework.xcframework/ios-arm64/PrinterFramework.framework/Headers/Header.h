a218157e8c5ba4957a8012cbab244908c5524e12
vong3432 <vongnyuksoon2000@gmail.com>
2024-11-29 18:26:52 +0800

    chore: rebuild framework

---
+++ PrinterFramework.xcframework/ios-arm64/PrinterFramework.framework/Headers/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  PrinterFramework
+//
+//  Created by nyuksoon.vong on 19/3/24.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <PrinterFramework/ePOS2.h>
+
+#endif /* Header_h */
---
+++ PrinterFramework.xcframework/ios-arm64/PrinterFramework.framework/Headers/PrinterFramework.h
@@ -0,0 +1,17 @@
+//
+//  PrinterFramework.h
+//  PrinterFramework
+//
+//  Created by nyuksoon.vong on 19/3/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <PrinterFramework/Header.h>
+
+//! Project version number for PrinterFramework.
+FOUNDATION_EXPORT double PrinterFrameworkVersionNumber;
+
+//! Project version string for PrinterFramework.
+FOUNDATION_EXPORT const unsigned char PrinterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
